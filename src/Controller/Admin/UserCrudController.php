5ee9fe76bfa50430eec1e3aaceeec5bf04346144
Mike Leman <mikeeleman@gmail.com>
2024-01-18 09:34:19 +0100

    User easy admin

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
@@ -20,8 +21,13 @@ class UserCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         yield IdField::new('id')->hideOnForm();
+        yield TextField::new('username');
         yield EmailField::new('email');
         yield TextField::new('password')->hideOnIndex()->hideOnForm()->hideOnDetail();
+        yield ArrayField::new('roles')->hideOnForm()->hideOnDetail()->hideOnIndex();
+        yield TextEditorField::new('details');
+        yield TextField::new('favorite_games');
+        yield TextField::new('play_schedule');
     }
 
 }
