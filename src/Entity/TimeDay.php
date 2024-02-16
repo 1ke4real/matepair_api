e279244834600273f3f127a4b04dbd7472d460b7
Mike Leman <mikeeleman@gmail.com>
2024-02-16 12:32:23 +0100

    Serilaze

--- src/Entity/TimeDay.php
+++ src/Entity/TimeDay.php
@@ -2,12 +2,17 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\TimeDayRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: TimeDayRepository::class)]
+#[ApiResource(
+    normalizationContext: ['groups' => ['read']],
+    denormalizationContext: ['groups' => ['write']],
+)]
 class TimeDay
 {
     #[ORM\Id]
