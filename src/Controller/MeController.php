2de4989df22fc91ada357bfa56aeccad17489caa
Mike Leman <mikeeleman@gmail.com>
2024-02-19 15:43:48 +0100

    edit me

--- src/Controller/MeController.php
+++ src/Controller/MeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Repository\UserRepository;
 use App\Service\TokenDecode;
+use Doctrine\Persistence\ObjectManager;
 use Lexik\Bundle\JWTAuthenticationBundle\Encoder\JWTEncoderInterface;
 use Lexik\Bundle\JWTAuthenticationBundle\Security\Guard\JWTTokenAuthenticator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -20,6 +21,22 @@ class MeController extends AbstractController
     {
         $jwt = $request->headers->get('Authorization');
         $user = $tokenDecode->decode($jwt);
+        return $this->json($user);
+    }
+
+    #[Route('api/me/update', name: 'app_me_update')]
+    public function edit(Request $request, TokenDecode $tokenDecode, ObjectManager $manager){
+        $jwt = $request->headers->get('Authorization');
+        $user = $tokenDecode->decode($jwt);
+        $data = json_decode($request->getContent(), true);
+        $user->setEmail($data['email']);
+        $user->setUsername($data['username']);
+        $user->setDetails($data['details']);
+        $user->setFavoriteGames($data['favorite_games']);
+        $user->addWeekDay($data['week_days']);
+        $user->addTimeDay($data['time_days']);
+        $manager->persist($user);
+        $manager->flush();
         return $this->json($user);
     }
 }
